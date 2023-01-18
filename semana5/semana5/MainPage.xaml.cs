03ea064dae01d66b175049a5e1a57607328d26c9
Kevin Torres <torreskevin_2015@outlook.com>
2023-01-17 20:33:30 -0500

    S6

---
+++ semana5/semana5/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace semana5
+{
+    public partial class MainPage : ContentPage
+    {
+        public const string Url = "http://192.168.21.145/uisrael2023/post.php";
+        private readonly HttpClient _client = new HttpClient();
+        private ObservableCollection<semana5.Ws.Datos> _post;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsertar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnGet_Clicked(object sender, EventArgs e)
+        {
+            var content = await _client.GetStringAsync(Url);
+            List<semana5.Ws.Datos> posts = JsonConvert.DeserializeObject<List<semana5.Ws.Datos>>(content);
+            _post = new ObservableCollection<semana5.Ws.Datos>(posts);
+            MyListView.ItemsSource = posts;
+        }
+    }
+}
---
+++ semana5/semana5/registro.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semana5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class registro : ContentPage
+    {
+        public registro()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsertar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                var parameters = new System.Collections.Specialized.NameValueCollection();
+                parameters.Add("codigo", txtCodigo.Text);
+                parameters.Add("nombre", txtNombre.Text);
+                parameters.Add("apellido", txtApellido.Text);
+                parameters.Add("edad", txtEdad.Text);
+                client.UploadValues("http://10.2.0.1/u_israel2023/post.php", "POST", parameters);
+             }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "cerrar");
+            }
+        }
+
+        private void btnCancelar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
